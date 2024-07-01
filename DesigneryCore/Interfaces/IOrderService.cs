3d84d2b8f67f1f70490f523c6f34e657a3cb7d1b
YMendelzon <159305317+YMendelzon@users.noreply.github.com>
2024-07-01 09:35:00 +0300

    -addOrder

---
+++ DesigneryCore/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using DesigneryCommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesigneryCore.Interfaces
+{
+    public interface IOrderService
+    {
+        IEnumerable<Order> GetHistoryOrdersByUserId(int userId);
+    }
+}
---
+++ DesingeryWeb/Controllers/OrsersController.cs
@@ -0,0 +1,26 @@
+﻿using DesigneryCommon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace DesingeryWeb.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly List<Order> orders = new List<Order>
+    {
+        new Order { Status = "", CreatedAt =new DateTime(2024, 6, 30, 14, 30, 0) , UserID = 1, TotalAmount =300, OrderID=1 },
+        new Order { Status = "", CreatedAt = new DateTime(2024, 6, 30, 14, 30, 0), UserID = 1, TotalAmount = 100, OrderID=2 }
+    };
+
+        //return for user his orders history
+        [HttpGet("{userId}")]
+        public ActionResult<IEnumerable<Order>> GetHistoryOrdersByUserId(int userId)
+        {
+            var userOrders = orders.Where(o => o.UserID == userId).ToList();
+            return Ok(userOrders);
+        }
+    }
+}
