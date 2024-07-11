67f273c58701d917c5155a128db7728d4e7c95e2
NoaElia <n0527102081@gmail.com>
2024-07-11 11:09:00 +0300

    Email update

--- DesigneryCommon/Models/EmailRequest.cs
+++ DesigneryCommon/Models/EmailRequest.cs
@@ -4,15 +4,15 @@ namespace DesigneryCommon.Models
 {
     public class EmailRequest
     {
-        public string Greeting { get; set; } // ברכה מותאמת אישית
+        public string? Greeting { get; set; } // ברכה מותאמת אישית
 
         public string? ToAddress { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
         public bool IsBodyHtml { get; set; }
         
-        public List<IFormFile> Attachments { get; set; }
-        public List<string> EmailList { get; set; }
+        public List<IFormFile>? Attachments { get; set; }
+        public List<string>? EmailList { get; set; }
 
 
     }
