0a326654a7a7356d99cc68f2f0f21c94ede5911b
Sara Sofer <sr6737543@gmail.com>
2024-08-10 23:48:26 +0300

    מייל לאיפוס

--- DesigneryCore/Services/GmailSmtpClientService.cs
+++ DesigneryCore/Services/GmailSmtpClientService.cs
@@ -13,6 +13,9 @@ using Microsoft.AspNetCore.Http;
 using static Org.BouncyCastle.Math.EC.ECCurve;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
+using iText.Layout.Element;
+using iText.Layout;
+using Org.BouncyCastle.Crypto.Macs;
 
 namespace DesigneryCore.Services
 {
@@ -23,7 +26,7 @@ namespace DesigneryCore.Services
         private readonly string gmailAddress;
         private readonly string gmailPassword;
         private readonly IConfiguration _config;
-        private readonly string resetLinkBaseUrl = "https://designeryback-ou6w.onrender.com/#/myResetPasswordLink";
+        private readonly string resetLinkBaseUrl = "https://engel.diversitech.co.il/#/myResetPasswordLink";
 
 
         public GmailSmtpClientService(string gmailAddress, string gmailPassword, IConfiguration configuration)
@@ -85,7 +88,7 @@ namespace DesigneryCore.Services
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress("no-reply@Designery.com", gmailAddress));
             message.To.Add(new MailboxAddress("", toAddress));
-            message.Subject = "Email to reset your password";
+            message.Subject = "בקשה לאיפוס סיסמה";
             ///
 
             //check Email exists
@@ -100,9 +103,27 @@ namespace DesigneryCore.Services
                 toAddress
                );
             var linkResetPas = $"{resetLinkBaseUrl}/{token}";
+ 
+            string body = $@"
+    <div style='text-align: center; font-family: Arial, sans-serif; color: #4A4A4A;'>
+        <h1 style='font-size: 28px; color: #E53D83;'>בהוקרה</h1>
+        <h1 style='font-size: 24px; color: #555555;'>איפוס סיסמה</h1>
+        <p style='font-size: 12px;'>קבלנו בקשה לאיפוס סיסמה עבור</p>
+        <p style='font-size: 11px; font-weight: bold; color: #555555;'>{toAddress}</p>
 
+        <a href='{linkResetPas}' style='display: inline-block; padding: 10px 20px; font-size: 16px; color: #ffffff; background-color: #FF007A; text-decoration: none; border-radius: 5px; margin-top: 20px;'>
+            מעבר לאיפוס הסיסמה
+        </a>
+        <p style='font-size: 12px;'>קישור זה תקף ל 8 דקות</p>
 
-            var bodyBuilder = new BodyBuilder { HtmlBody = null, TextBody = linkResetPas };
+    </div>";
+
+            var bodyBuilder = new BodyBuilder { HtmlBody = body , TextBody = null };
+                
+                
+                
+                
+               
             message.Body = bodyBuilder.ToMessageBody();
             try
             {
